a4bef7bd9ec781268002d1b1870d1109472fd2a8
edksam <62302678+edksam@users.noreply.github.com>
2021-04-19 13:58:26 +0100

    After Classes and Objects Exercises. Classes Shop and Product created.

--- src/com/capgemini/app/Shop.java
+++ src/com/capgemini/app/Shop.java
@@ -1,7 +1,22 @@
 package com.capgemini.app;
 
+import com.capgemini.data.Product;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * {@code Shop} class represents an application that manages Products
+ * @version 4.0
+ * @author Ebenezer
+ */
+
 public class Shop {
     public static void main(String[] args) {
-
+      Product p1 = new Product();
+      p1.setId(101);
+      p1.setName("Tea");
+      p1.setPrice(BigDecimal.valueOf(1.99));
+      System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " +p1.getDiscount());
     }
 }
